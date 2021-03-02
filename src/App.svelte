71624a6233d5cffba289fd0f05c97a80e4d6240a
Conner Chappell <conner.chappell123@gmail.com>
2021-03-01 22:07:22 -0700

    Installed and imported lodash

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script>
+	import _ from 'lodash'
 	export let name;
+
+	
+
+
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>{name}</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
